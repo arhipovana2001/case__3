d9b43d567bb9c2ca5b1005ba0e256522ddd07f93
arhipovana2001 <55373084+arhipovana2001@users.noreply.github.com>
2019-10-29 08:46:21 +0700

    Update local_english.py

--- local_english.py
+++ local_english.py
@@ -1,1 +1,16 @@
-#TODO: LOCAL_ENGLISH_NASTYA
+# Localization file (english).
+YEARS = "Term of capital placement (years): "
+ERROR_1 = "Error. Enter the year again:"
+INITIAL_CAPITAL = "Initial capital ($): "
+ERROR_2 = "Error. Enter the initial capital again ($):"
+PERCENT = "Interest rate (%/month): "
+ERROR_3 = "Error. Enter the interest rate again"
+INVESTMENT_INFUSION = "Investment injections ($/month): "
+ERROR_4 = "Error. Enter the investment infusion again: "
+PRINT_YEAR = 'year'
+MONTH = 'month'
+PRINT_INITIAL_CAPITAL = 'initial'
+PRINT_INITIAL_CAPITAL_1 = 'capital'
+PRINT_INCOME = 'income'
+PRINT_INVESTMENT_INFUSION = 'investment'
+PRINT_INVESTMENT_INFUSION_1 = 'infusion'
