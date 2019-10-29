62c0df8f63a1cbe2019a3ccb61ddb24adcb7db7c
LidaRevtova <55373057+LidaRevtova@users.noreply.github.com>
2019-10-29 08:48:45 +0700

    GERMAN

--- local_germany.py
+++ local_germany.py
@@ -1,1 +1,17 @@
 #TODO: LOCAL_GERMANY_LIDA
+
+# Localization file (germany).
+YEARS = "Laufzeit der Kapitalplatzierung (Jahre): "
+ERROR_1 = "Der Fehler. Geben Sie das Jahr erneut ein: "
+INITIAL_CAPITAL = "Anfangskapital ($)："
+ERROR_2 = "Der Fehler. Geben Sie das Anfangskapital ($) erneut ein: "
+PERCENT = "Zinssatz (% / Monat): "
+ERROR_3 = "Der Fehler. Geben Sie den Zinssatz (% / Monat) erneut ein："
+INVESTMENT_INFUSION = "Der Fehler. Geben Sie den Zinssatz (% / Monat) erneut ein："
+ERROR_4 = "Investitionsspritzen ($ / Monat)："
+PRINT_YEAR = "Jahr"
+MONTH = "Monat"
+PRINT_INITIAL_CAPITAL = "Hauptstadt"
+PRINT_INCOME = "Menge %"
+PRINT_INVESTMENT_INFUSION = "Investition"
+PRINT_INVESTMENT_INFUSION_1 = "Infusion"
